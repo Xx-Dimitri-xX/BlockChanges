028f15f5e0c7a6d9555e1be3b1e37cdc9481f9cf
Xx-Dimitri-xX <dimitri.namour2@gmail.com>
2025-02-17 14:37:19 +0100

    Créé .env & modifié hardhat config

--- quiz-capitales-dapp/hardhat.config.js
+++ quiz-capitales-dapp/hardhat.config.js
@@ -1,4 +1,12 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+
 module.exports = {
-  solidity: "0.8.28",
+    solidity: "0.8.21",
+    networks: {
+        hardhat: {},
+        sepolia: {
+            url: process.env.SEPOLIA_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY]
+        }
+    }
 };
